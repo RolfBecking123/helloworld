a9bc6f7c1df5bdb876fc6408bc1b49971d053f37
RolfBecking123 <40362284+RolfBecking123@users.noreply.github.com>
2018-06-20 17:27:53 +0200

    Initial

---
+++ imptest2/Program1.cbl
@@ -0,0 +1,14 @@
+       program-id. Program1 as "Program1".
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+           display "hello rolf".
+
+           goback.
+
+       end program Program1.
